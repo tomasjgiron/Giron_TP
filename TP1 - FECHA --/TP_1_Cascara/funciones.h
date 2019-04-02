86d7054fa398aa28288ca4351c839d53d475d9b8
tomasjgiron <47337943+tomasjgiron@users.noreply.github.com>
2019-04-02 20:07:38 -0300

    Primer trabajo práctico - Calculadora

---
+++ TP1 - FECHA --/TP_1_Cascara/funciones.h
@@ -0,0 +1,20 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+long utn_factorial(int numberA);
+
+int utn_sum(int numberA, int numberB, char* msg);
+
+int utn_substraction(int numberA, int numberB, char* msg);
+
+int utn_multiply(int numberA, int numberB, char* msg);
+
+float utn_division(int numberA, int numberB, char* msg, char* msgError);
+
+int utn_getInt(int *pNum, char* msg, char* msgError);
+
+int utn_getFloat(float *pNum, char* msg, char* msgError);
+
+float utn_calculateAll(int numberA, int numberB);
+
+#endif // FUNCIONES_H_INCLUDED
